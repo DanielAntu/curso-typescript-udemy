b552e22ef085afc3b3281e05a5985c699e84e1b0
DanielAntunes <antunesdaniel083@gmail.com>
2023-04-20 18:03:48 -0300

    Atualização

--- 8_CLASSES/index.ts
+++ 8_CLASSES/index.ts
@@ -198,3 +198,170 @@ class Nova extends Base {
 const myObject = new Nova();
 
 myObject.someMethod();
+
+// 11 - public
+class C {
+    public x = 10;
+}
+
+class D extends C {}
+
+const cInstance = new C();
+
+console.log(cInstance.x);
+
+const dInstance = new D();
+
+console.log(dInstance.x);
+
+// 12 - protected
+class E {
+    protected x = 10;
+
+    protected protectedMethod() {
+        console.log("Este metodo é protegido");
+    }
+}
+
+class F extends E {
+    showX() {
+        console.log("X: " + this.x);
+    }
+
+    showProtectedMethod() {
+        this.protectedMethod();
+    }
+}
+
+const finstance = new F();
+
+finstance.showX();
+finstance.showProtectedMethod();
+
+// console.log(finstance.x)
+
+// 13 - private
+class Private {
+    private name = "Private";
+
+    showName() {
+        return this.name;
+    }
+}
+
+const pObject = new Private();
+
+// console.log(pObject.name)
+
+console.log(pObject.showName());
+
+// 14 - static members
+class StaticMembers {
+    static prop = "Teste static";
+
+    static staticMember() {
+        console.log("Este é um método estático");
+    }
+}
+
+console.log(StaticMembers.prop);
+StaticMembers.staticMember();
+
+// 15 - generic class
+class Item<T, U> {
+    first;
+    second;
+
+    constructor(first: T, second: U) {
+        this.first = first;
+        this.second = second;
+    }
+
+    get showFirst() {
+        return `O first é: ${this.first}`;
+    }
+}
+
+const newItem = new Item("caixa", "surpresa");
+
+console.log(newItem);
+
+console.log(newItem.showFirst);
+
+// 16 - Parameters properties
+class ParameterProperties {
+    constructor(
+        public name: string,
+        private qty: number,
+        private price: number
+    ) {
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    get showQty() {
+        return `Quantidade total: ${this.qty}`;
+    }
+
+    get showPrice() {
+        return `Preço do produto: ${this.price}`;
+    }
+}
+
+const newShirt = new ParameterProperties("Camisa", 5, 19.99);
+console.log(newShirt.name);
+console.log(newShirt.showPrice);
+console.log(newShirt.showQty);
+
+// 17 - class expressions
+const myClass = class<T> {
+    name;
+
+    constructor(name: T) {
+        this.name = name;
+    }
+};
+
+const pessoa = new myClass("Jones");
+
+console.log(pessoa);
+
+console.log(pessoa.name);
+
+// 18 - abstract class
+abstract class AbstractClass {
+    abstract showName(): void;
+}
+
+// const newObj = new AbstractClass()
+
+class AbstractExample extends AbstractClass {
+    name;
+
+    constructor(name: string) {
+        super();
+        this.name = name;
+    }
+
+    showName(): void {
+        console.log(`O nome é: ${this.name}`);
+    }
+}
+
+const newAbstractObject = new AbstractExample("Josias");
+
+newAbstractObject.showName();
+
+// 19 - relações entre classes
+
+class Dog {
+    name!: string;
+}
+
+class Cat {
+    name!: string;
+}
+
+const doguinho: Dog = new Cat();
+console.log(doguinho);
