e864086fe023a68468ae9c1fba0f2ec14d60a2f1
DanielAntunes <antunesdaniel083@gmail.com>
2023-04-26 18:36:58 -0300

    Atualização

---
+++ 12_todo_react/src/App.tsx
@@ -0,0 +1,19 @@
+import styles from "./App.module.css";
+
+// components
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <main className={styles.main}>
+                <h1>Conteudo...</h1>
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
