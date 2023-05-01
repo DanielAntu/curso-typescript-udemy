472347074f80d79573b843a03071b0b29f7de10f
DanielAntunes <antunesdaniel083@gmail.com>
2023-05-01 19:12:40 -0300

    Atualização

---
+++ 13_express_com_ts/src/app.ts
@@ -0,0 +1,79 @@
+// 1 - iniciando projeto
+//console.log("express + TS!");
+
+// 2 - init express
+import express, { Request, Response } from "express";
+
+const app = express();
+
+// 3 - rotas com post
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+    return res.send("Hello Express!");
+});
+
+// 3 - rotas com post
+app.post("/api/product", (req, res) => {
+    console.log(req.body);
+
+    return res.send("Produto adicionado!");
+});
+
+// 4 - rotas para todos os verbos
+app.all("/api/product/check", (req, res) => {
+    //req.method = verbo HTTP
+
+    if (req.method === "POST") {
+        return res.send("Inseriu algum registro");
+    } else if (req.method === "GET") {
+        return res.send("Leu algum registro");
+    } else {
+        return res.send("Não podemos realizar esta operação!");
+    }
+});
+
+// 5 - interface do express
+app.get("/api/interfaces", (req: Request, res: Response) => {
+    return res.send("Ultilizando as interfaces");
+});
+
+// 6 - enviando json
+app.get("/api/json", (req: Request, res: Response) => {
+    return res.json({
+        name: "Shirt",
+        price: 30.0,
+        color: "Blue",
+        sizes: ["P", "M", "G"],
+    });
+});
+
+// 7 - router parameter
+app.get("/api/product/:id", (req: Request, res: Response) => {
+    console.log(req.params);
+
+    const id = req.params.id;
+
+    if (id === "1") {
+        const product = {
+            id: 1,
+            name: "Boné",
+            price: 10,
+        };
+
+        return res.json(product);
+    } else {
+        return res.send("Produto não encontrado!");
+    }
+});
+
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+    console.log(req.params);
+
+    const productId = req.params.id;
+    const reviewId = req.params.reviewId;
+
+    return res.send(`Acessando a review ${reviewId} do produto ${productId}`);
+});
+
+app.listen(3000, () => console.log("Aplicação de ts + express funcionando"));
