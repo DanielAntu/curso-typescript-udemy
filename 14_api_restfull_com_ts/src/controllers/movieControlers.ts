30ad768eedf5d1ef2a6f8c13394bab0b366a292a
DanielAntunes <antunesdaniel083@gmail.com>
2023-05-08 17:22:31 -0300

    Atualização

--- 14_api_restfull_com_ts/src/controllers/movieControlers.ts
+++ 14_api_restfull_com_ts/src/controllers/movieControlers.ts
@@ -14,5 +14,70 @@ export async function createMovie(req: Request, res: Response) {
         return res.status(201).json(movie);
     } catch (error: any) {
         logger.error(`Erro no sistema: ${error.message}`);
+        return res.status(500).json({ error: "Por favor, tente mais tarde" });
+    }
+}
+
+export async function findMovieById(req: Request, res: Response) {
+    try {
+        const id = req.params.id;
+        const movie = await movieModel.findById(id);
+
+        if (!movie) {
+            return res.status(404).json({ error: "O filme não existe." });
+        }
+
+        return res.status(200).json(movie);
+    } catch (error: any) {
+        logger.error(`Erro no sistema: ${error.message}`);
+        return res.status(500).json({ error: "Por favor, tente mais tarde" });
+    }
+}
+
+export async function getAllMovies(req: Request, res: Response) {
+    try {
+        const movies = await movieModel.find();
+        return res.status(200).json(movies);
+    } catch (error: any) {
+        logger.error(`Erro no sistema: ${error.message}`);
+        return res.status(500).json({ error: "Por favor, tente mais tarde" });
+    }
+}
+
+export async function removeMovie(req: Request, res: Response) {
+    try {
+        const id = req.params.id;
+        const movie = await movieModel.findById(id);
+
+        if (!movie) {
+            return res.status(404).json({ error: "O filme não existe." });
+        }
+
+        await movie.deleteOne();
+
+        return res.status(200).json({ msg: "Filme removido com sucesso!" });
+    } catch (error: any) {
+        logger.error(`Erro no sistema: ${error.message}`);
+        return res.status(500).json({ error: "Por favor, tente mais tarde" });
+    }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+    try {
+        const id = req.params.id;
+        const data = req.body;
+
+        const movie = await movieModel.findById(id);
+
+        if (!movie) {
+            return res.status(404).json({ error: "O filme não existe." });
+        }
+
+        await movieModel.updateOne({ _id: id }, data);
+
+        return res.status(200).json(data);
+    } catch (error: any) {
+        logger.error(`Erro no sistema: ${error.message}`);
+        return res.status(500).json({ error: "Por favor, tente mais tarde" });
     }
 }
--- 14_api_restfull_com_ts/src/router.ts
+++ 14_api_restfull_com_ts/src/router.ts
@@ -1,8 +1,15 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/movieControlers";
+import {
+    createMovie,
+    findMovieById,
+    getAllMovies,
+    removeMovie,
+    updateMovie,
+} from "./controllers/movieControlers";
 
 // Validations
 import { validate } from "./middlewares/handleValidate";
+import { movieCreateValidation } from "./middlewares/movieValidator";
 
 const router = Router();
 
@@ -10,4 +17,8 @@ export default router
     .get("/test", (req: Request, res: Response) => {
         res.status(200).send("API Working");
     })
-    .post("/movie", validate, createMovie);
+    .post("/movie", movieCreateValidation(), validate, createMovie)
+    .get("/movie/:id", findMovieById)
+    .get("/movie", getAllMovies)
+    .delete("/movie/:id", removeMovie)
+    .patch("/movie/:id", movieCreateValidation(), validate, updateMovie);
